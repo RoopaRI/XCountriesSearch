7db2fb58009ca24f067e0cb47d8323048f340b41
Roopa S <roopa31113@gmail.com>
2024-08-09 17:38:00 +0530

    Impleamented debouncing

--- src/CountriesSearch.jsx
+++ src/CountriesSearch.jsx
@@ -1,6 +1,6 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 import axios from "axios";
-import "./CountriesSearch.css"
+import "./CountriesSearch.css";
 
 function CountriesSearch() {
   const [flags, setFlags] = useState([]);
@@ -9,11 +9,29 @@ function CountriesSearch() {
   const fetchData = async () => {
     try {
       const response = await axios.get("https://restcountries.com/v3.1/all");
+      console.log(response);
       setFlags(response.data); // Set the entire array of country data
     } catch (error) {
       console.error("Error fetching data:", error);
     }
   };
+
+  const debounce = (func, delay) => {
+    let timeoutId;
+    return (...args) => {
+      if (timeoutId) clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        func(...args);
+      }, delay);
+    };
+  };
+
+  const handleSearch = useCallback(
+    debounce((query) => {
+      setSearchQuery(query);
+    }, 300),
+    []
+  );
 
   const filteredCountries = flags.filter((country) =>
     country.name.common.toLowerCase().includes(searchQuery.toLowerCase())
@@ -21,28 +39,27 @@ function CountriesSearch() {
 
   useEffect(() => {
     fetchData(); // Fetch data when the component mounts
-  },[]);
+  }, []);
 
   return (
     <div>
-        <div className="topnav">
-            <input
-                className="Search"
-                type="text"
-                placeholder="Search for countries"
-                value={searchQuery}
-                onChange={(e) => setSearchQuery(e.target.value)} // Update search query
-            />
-        </div>
-        <div className="countries">
-            {filteredCountries.map((country) => (
-                    <div className="countryCard" key={country.alpha3Code}>
-                        <img src={country.flags.png} alt={country.flags.alt} />
-                        <h2>{country.name.common}</h2>
-                    </div>
-            ))}
-        </div>
-    </div>   
+      <div className="topnav">
+        <input
+          className="Search"
+          type="text"
+          placeholder="Search for countries"
+          onChange={(e) => handleSearch(e.target.value)} // Use debounced search handler
+        />
+      </div>
+      <div className="countries">
+        {filteredCountries.map((country) => (
+          <div className="countryCard" key={country.alpha3Code}>
+            <img src={country.flags.png} alt={country.flags.alt} />
+            <h2>{country.name.common}</h2>
+          </div>
+        ))}
+      </div>
+    </div>
   );
 }
 
